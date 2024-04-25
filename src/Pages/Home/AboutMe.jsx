f3ee806b55d19d8365327f4b1c17ec8f804ebef6
otienorichard <otienorichard505@gmail.com>
2024-04-25 20:31:05 +0300

    debugging

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -2,7 +2,7 @@
   return (
     <section id="AboutMe" className="about--section">
       <div className="about--section--img">
-        <img src="./img/about-me.png" alt="About Me" />
+        <img src="./img/hero_img.jpg" alt="About Me" />
       </div>
       <div className="hero--section--content--box about--section--box">
         <div className="hero--section--content">
@@ -13,7 +13,9 @@
            and alternative education from ALX/Holberton School.
            My experience includes working on various projects, both independently and collaboratively, 
            honing my skills through internships and roles at companies like Italanta, Digital Divide Data, 
-           Limax, and Impact Outsourcing AI. While proficient in software engineering, I am also eager to explore the intersection of machine learning with my current expertise. Known for my adaptability, diligence, and problem-solving abilities, I am committed to delivering innovative solutions in the tech industry."
+           Limax, and Impact Outsourcing AI. While proficient in software engineering, I am also eager to 
+           explore the intersection of machine learning with my current expertise. Known for my adaptability,
+            diligence, and problem-solving abilities, I am committed to delivering innovative solutions in the tech industry.
 
           </p>
          
